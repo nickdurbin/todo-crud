cbc1ef61ad79fb3cb5823a8cf180c49fada10f7b
ndurbin <nick.durbin@gmail.com>
2019-09-16 01:36:08 -0400

    Added edit functionality for full CRUD.

--- src/components/Tasks/TaskForm/TaskForm.js
+++ src/components/Tasks/TaskForm/TaskForm.js
@@ -1,20 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
-function TaskForm({ setTasks, tasks, history }) {
+function TaskForm({ setTasks, tasks, history, edit, match: { params: { id } } }) {
   const [formValues, setFormValues] = useState({
     id: Date.now(),
     task: '',
     isCompleted: false
   })
 
+  useEffect(() => {
+    if (edit) {
+      const editTask = tasks.filter(task => task.id.toString() === id)[0]
+      setFormValues(editTask);
+    }
+  }, [])
+
   const handleChange = (e) => {
     setFormValues({...formValues, [e.target.name]: e.target.value})
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+  function updateTask(taskId) {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return formValues;
+      } else {
+        return task;
+      }  
+    })
+    setTasks(updatedTasks);
+  }
+
+  function addTask() {
     setFormValues({...formValues, id: Date.now()})
     setTasks([...tasks, formValues])
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    edit ? updateTask(formValues.id) : addTask();
     history.push('/')
   }
 
